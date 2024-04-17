dfe323cb67cc878b472dc5f9a666253fa9533393
Diego Ramon <Diegoramon@live.nl>
2024-04-17 16:31:54 +0200

    Long hold work around

--- js/script.js
+++ js/script.js
@@ -129,3 +129,34 @@ function Randomise(){
     ul.appendChild(ul.children[Math.random()*i|0])
   }    
 }
+
+function absorbEvent_(event) {
+  var e = event || window.event;
+  e.preventDefault && e.preventDefault();
+  e.stopPropagation && e.stopPropagation();
+  e.cancelBubble = true;
+  e.returnValue = false;
+  return false;
+}
+
+function preventLongPressMenu(node) {
+  node.ontouchstart = absorbEvent_;
+  node.ontouchmove = absorbEvent_;
+  node.ontouchend = absorbEvent_;
+  node.ontouchcancel = absorbEvent_;
+}
+
+function init() {
+  preventLongPressMenu(document.getElementById('musician1'));
+  preventLongPressMenu(document.getElementById('musician2'));
+  preventLongPressMenu(document.getElementById('musician3'));
+  preventLongPressMenu(document.getElementById('instrument1'));
+  preventLongPressMenu(document.getElementById('instrument2'));
+  preventLongPressMenu(document.getElementById('instrument3'));
+}
+
+window.oncontextmenu = function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+};
